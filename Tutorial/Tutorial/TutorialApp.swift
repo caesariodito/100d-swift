4ca871f15c4147ce534e48409eda90563d860573
Macbook Air-Caesario Dito <caesariodito@gmail.com>
2024-05-02 16:02:39 +0700

    feat: init swiftui project for tutorial

---
+++ Tutorial/Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by MacBook Air on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
